b34108896b667462a270dbb1af4facfe9e7737c6
Gabriel Christino <gabriel.christino@gmail.com>
2025-04-26 17:15:09 -0300

    feat: exercise 1

---
+++ src/Exercise1a.java
@@ -0,0 +1,11 @@
+public class Exercise1a {
+    public static void main(String[] args) {
+        int x = 1;
+        while (x < 10) {
+            x = x + 1; // falta esta linha no exercício
+            if(x > 3) {
+                System.out.println("bix X");
+            }
+        }
+    }
+}
